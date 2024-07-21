4333578a2ce70cc77aa41d900546accd64f48ecc
helixoid <ayushrajmth91@gmail.com>
2024-07-21 17:25:06 +0530

    Hyprland Development Branch

--- configuration.nix
+++ configuration.nix
@@ -94,4 +94,9 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
+
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
 }
