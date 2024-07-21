78e17b15b2bf3a1d9ffeffbba017484f5b9a96fa
helixoid <ayushrajmth91@gmail.com>
2024-07-21 16:30:36 +0530

    Hyprland Development Branch

--- flake.nix
+++ flake.nix
@@ -1,14 +1,21 @@
 {
-  description = "XNM's NixOS Configuration";
+  description = "Helixoid's Nixos Configuration with Hyprland :)";
+  
+  nixConfig = {
+    extra-substituters = ["https://hyprland.cachix.org"];
+    extra-trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
 
   inputs = {
       nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+      hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
   };
 
-  outputs = { nixpkgs, ... } @ inputs:
+  outputs = { self, nixpkgs, hyprland, ... } @ inputs:
   {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs; };
+        config.allowUnfree = true;
       modules = [
         ./configuration.nix
         ./hardware-configuration.nix
--- hyprland.nix
+++ hyprland.nix
@@ -4,13 +4,10 @@
   # Enable Hyprland
   programs.hyprland = {
       enabled = true;
-      package = 
+      package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland; 
   }
-
-
-
- # environment.sessionVariables.NIXOS_OZONE_WL = "1";
-# # environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
+  # environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  # environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
 
   environment.systemPackages = with pkgs; [
     hyprpicker
